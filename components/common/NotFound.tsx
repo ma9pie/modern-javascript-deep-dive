c180f32813c5ee3101dd62cbdf424584d34696ed
ma9pie <ma9pie.dev@gmail.com>
2024-01-03 22:01:03 +0900

    feat : NotFound 컴포넌트, 404 페이지 추가

---
+++ components/common/NotFound.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import tw, { styled } from 'twin.macro';
+
+import Text from '@/components/common/Text';
+
+const NotFound = () => {
+  return (
+    <Wrapper>
+      <Text bold center xl9>
+        404
+      </Text>
+      <Text bold center xl6>
+        Not found
+      </Text>
+    </Wrapper>
+  );
+};
+
+export default NotFound;
+
+const Wrapper = styled.div`
+  ${tw`flex flex-col justify-center items-center h-full`};
+`;
---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import NotFound from '@/components/common/NotFound';
+import Layout from '@/components/layouts/Layout';
+
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <NotFound></NotFound>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
