d9c6f7ece675e1d67f0314e0f31f8661429d585f
ma9pie <ma9pie.dev@gmail.com>
2024-01-04 19:22:17 +0900

    feat : Home route 생성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,36 @@
+import Link from 'next/link';
 import React from 'react';
 import tw, { styled } from 'twin.macro';
 
+import Flex from '@/components/common/Flex';
+import Text from '@/components/common/Text';
 import Layout from '@/components/layouts/Layout';
+import { CHAPTER } from '@/constants';
 
 const Home = () => {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      <Container>
+        {CHAPTER.map(({ id, title }) => (
+          <Link className="w-fit" key={id} href={`/chapter/${id}`}>
+            <Flex gap={12}>
+              <Text lg blue400>{`제 ${id}장 `}</Text>
+              <Text lg blue400>
+                {title}
+              </Text>
+            </Flex>
+          </Link>
+        ))}
+      </Container>
+    </Layout>
+  );
 };
 
 export default Home;
 
-const Wrapper = styled.div`
-  ${tw``};
+const Container = styled.div`
+  ${tw`flex flex-col gap-4`};
+`;
+const LinkText = styled.div`
+  ${tw`text-lg`};
 `;
