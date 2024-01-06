4dae36f5632226eb759ee77feb5b07c156bb2185
ma9pie <ma9pie.dev@gmail.com>
2024-01-07 03:02:33 +0900

    config : 예제 코드 표시를 위한 uglify 비활성화

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  // 예제 코드 표시를 위한 uglify 비활성화
+  webpack(config) {
+    return {
+      ...config,
+      optimization: {
+        minimize: false,
+      },
+    };
+  },
 };
 
 module.exports = nextConfig;
