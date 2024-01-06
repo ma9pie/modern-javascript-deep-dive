cf27f1bdea0e5246a30a0a97ca41298caf5342d5
ma9pie <ma9pie.dev@gmail.com>
2024-01-07 04:21:49 +0900

    test : pwa icon test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,120 +1,127 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+const TITLE = 'Modern Javascript Deep Dive';
+const DESCRIPTION = 'javscript study';
+const AUTHOR = 'ma9pie';
+const KEYWORDS = 'javscript';
+const URL = 'https://modern-javascript-deep-dive.vercel.app/';
+
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ko">
       <Head>
         {/* 메타 태그 설정 */}
-        <meta name="description" content="Modern Javascript Deep Dive" />
         <meta charSet="utf-8" />
-        <meta name="author" content="ma9pie" />
+        <meta name="description" content={DESCRIPTION} />
+        <meta name="author" content={AUTHOR} />
         <meta name="image" content="/images/logo/logo.svg" />
         <meta name="robots" content="index, follow" />
-        <meta name="keywords" content="javascript" />
+        <meta name="keywords" content={KEYWORDS} />
 
         {/* Facebook Meta Tags */}
-        <meta
-          property="og:url"
-          content="https://modern-javascript-deep-dive.vercel.app/"
-        />
-        <meta property="og:site_name" content="Modern Javascript Deep Dive" />
-        <meta property="og:title" content="Modern Javascript Deep Dive" />
+        <meta property="og:url" content={URL} />
+        <meta property="og:site_name" content={TITLE} />
+        <meta property="og:title" content={TITLE} />
         <meta property="og:image" content="/images/logo/logo.svg" />
-        <meta property="og:description" content="Modern Javascript Deep Dive" />
+        <meta property="og:description" content={DESCRIPTION} />
         <meta property="og:type" content="website" />
 
         {/* Twitter Meta Tags */}
-        <meta name="twitter:card" content="Modern Javascript Deep Dive" />
-        <meta
-          name="twitter:url"
-          content="https://modern-javascript-deep-dive.vercel.app/"
-        />
-        <meta name="twitter:site_name" content="Modern Javascript Deep Dive" />
-        <meta name="twitter:title" content="Modern Javascript Deep Dive" />
+        <meta name="twitter:card" content={TITLE} />
+        <meta name="twitter:url" content={URL} />
+        <meta name="twitter:site_name" content={TITLE} />
+        <meta name="twitter:title" content={TITLE} />
         <meta name="twitter:image" content="/images/logo/logo.svg" />
-        <meta
-          name="twitter:description"
-          content="Modern Javascript Deep Dive"
-        />
+        <meta name="twitter:description" content={DESCRIPTION} />
         <meta name="twitter:type" content="website" />
 
-        {/* https://www.favicon-generator.org/ */}
+        {/* https://favicomatic.com/ */}
         <link
-          rel="apple-touch-icon"
+          rel="apple-touch-icon-precomposed"
           sizes="57x57"
-          href="/icons/apple-icon-57x57.png"
+          href="/icons/apple-touch-icon-57x57.png"
         />
         <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="/icons/apple-icon-60x60.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="/icons/apple-icon-72x72.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="/icons/apple-icon-76x76.png"
-        />
-        <link
-          rel="apple-touch-icon"
+          rel="apple-touch-icon-precomposed"
           sizes="114x114"
-          href="/icons/apple-icon-114x114.png"
+          href="/icons/apple-touch-icon-114x114.png"
         />
         <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/icons/apple-icon-120x120.png"
+          rel="apple-touch-icon-precomposed"
+          sizes="72x72"
+          href="/icons/apple-touch-icon-72x72.png"
         />
         <link
-          rel="apple-touch-icon"
+          rel="apple-touch-icon-precomposed"
           sizes="144x144"
-          href="/icons/apple-icon-144x144.png"
+          href="/icons/apple-touch-icon-144x144.png"
         />
         <link
-          rel="apple-touch-icon"
+          rel="apple-touch-icon-precomposed"
+          sizes="60x60"
+          href="/icons/apple-touch-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          sizes="120x120"
+          href="/icons/apple-touch-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          sizes="76x76"
+          href="/icons/apple-touch-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
           sizes="152x152"
-          href="/icons/apple-icon-152x152.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/icons/apple-icon-180x180.png"
+          href="/icons/apple-touch-icon-152x152.png"
         />
         <link
           rel="icon"
           type="image/png"
-          sizes="192x192"
-          href="/icons/android-icon-192x192.png"
+          href="/icons/favicon-196x196.png"
+          sizes="196x196"
         />
         <link
           rel="icon"
           type="image/png"
-          sizes="32x32"
-          href="/icons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
           href="/icons/favicon-96x96.png"
+          sizes="96x96"
         />
         <link
           rel="icon"
           type="image/png"
-          sizes="16x16"
+          href="/icons/favicon-32x32.png"
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
           href="/icons/favicon-16x16.png"
+          sizes="16x16"
         />
-        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/icons/favicon-128.png"
+          sizes="128x128"
+        />
+        <meta name="application-name" content={TITLE} />
         <meta name="msapplication-TileColor" content="#222222" />
+        <meta name="msapplication-TileImage" content="mstile-144x144.png" />
+        <meta name="msapplication-square70x70logo" content="mstile-70x70.png" />
         <meta
-          name="msapplication-TileImage"
-          content="/icons/ms-icon-144x144.png"
+          name="msapplication-square150x150logo"
+          content="mstile-150x150.png"
         />
-        <meta name="theme-color" content="#222222" />
+        <meta
+          name="msapplication-wide310x150logo"
+          content="mstile-310x150.png"
+        />
+        <meta
+          name="msapplication-square310x310logo"
+          content="mstile-310x310.png"
+        />
 
         {/* https://appsco.pe/developer/splash-screens */}
         <link
