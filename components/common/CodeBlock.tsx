253ee995b4b0c10175b5385158278ce3005b1413
ma9pie <ma9pie.dev@gmail.com>
2024-01-09 18:18:39 +0900

    fix : CodeBlock props 수정

--- components/common/CodeBlock.tsx
+++ components/common/CodeBlock.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { vs2015 } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 import tw, { styled } from 'twin.macro';
 
 interface Props {
   className?: string;
-  code: string;
+  fn?: () => void;
 }
 
-const CodeBlock = ({ className, code }: Props) => {
-  if (!code) return null;
+const CodeBlock = ({ className, fn }: Props) => {
+  useEffect(() => {
+    if (!fn) return;
+    console.log(fn.name);
+    fn();
+  }, []);
+
   return (
     <Wrapper className={className}>
       <SyntaxHighlighter language="javascript" style={vs2015}>
-        {code}
+        {String(fn)}
       </SyntaxHighlighter>
     </Wrapper>
   );
