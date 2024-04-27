158e3cde2d3bd40111d089fc1461407b0839e187
ma9pie <ma9pie.dev@gmail.com>
2024-04-27 16:59:59 +0900

    design : Header 레이아웃 조정

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -8,44 +8,46 @@ import { Flex, ResponsiveView, Text } from '@/components/common';
 const Header = () => {
   return (
     <Wrapper>
-      <Link href="/" passHref>
-        <ResponsiveView
-          mobile={
-            <Flex items="center" gap={8}>
-              <Image
-                src="/images/logo/logo.svg"
-                width={24}
-                height={24}
-                alt="logo"
-              ></Image>
-              <Text lg bold>
-                모던 자바스크립트 Deep Dive
-              </Text>
-            </Flex>
-          }
-          desktop={
-            <Flex items="center" gap={8}>
-              <Image
-                src="/images/logo/logo.svg"
-                width={32}
-                height={32}
-                alt="logo"
-              ></Image>
-              <Flex items="end" gap={8}>
-                <Text _2xl bold>
-                  모던
-                </Text>
-                <Text _2xl bold>
-                  자바스크립트
-                </Text>
-                <Text xl semibold>
-                  Deep Dive
+      <Content>
+        <Link href="/" passHref>
+          <ResponsiveView
+            mobile={
+              <Flex items="center" gap={8}>
+                <Image
+                  src="/images/logo/logo.svg"
+                  width={24}
+                  height={24}
+                  alt="logo"
+                ></Image>
+                <Text lg bold>
+                  모던 자바스크립트 Deep Dive
                 </Text>
               </Flex>
-            </Flex>
-          }
-        ></ResponsiveView>
-      </Link>
+            }
+            desktop={
+              <Flex items="center" gap={8}>
+                <Image
+                  src="/images/logo/logo.svg"
+                  width={32}
+                  height={32}
+                  alt="logo"
+                ></Image>
+                <Flex items="end" gap={8}>
+                  <Text _2xl bold>
+                    모던
+                  </Text>
+                  <Text _2xl bold>
+                    자바스크립트
+                  </Text>
+                  <Text xl semibold>
+                    Deep Dive
+                  </Text>
+                </Flex>
+              </Flex>
+            }
+          ></ResponsiveView>
+        </Link>
+      </Content>
     </Wrapper>
   );
 };
@@ -54,4 +56,7 @@ export default Header;
 
 const Wrapper = styled.div`
   ${tw`flex items-center h-20 border-b border-solid border-b-neutral-700 px-6`};
+`;
+const Content = styled.div`
+  ${tw`max-w-7xl w-full mx-auto`};
 `;
